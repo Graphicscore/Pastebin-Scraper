8ea039a92c1e2875320e958d15a3adfb13a152a6
Dominik Louven <louven.dominik@gmail.com>
2018-02-27 14:05:55 +0100

    Changed Paste object to jsonobject constructor

--- src/de/graphicscore/pastebinscraper/Paste.java
+++ src/de/graphicscore/pastebinscraper/Paste.java
@@ -2,7 +2,7 @@ package de.graphicscore.pastebinscraper;
 
 import org.json.JSONObject;
 
-public class Paste extends JSONObject{
+public class Paste{
 
     private static final String
     KEY_USER = "user",
@@ -15,15 +15,32 @@ public class Paste extends JSONObject{
     KEY_FULL_URL = "full_url",
     KEY_SCRAPE_URL = "scrape_url";
 
-    public String getUser() { return getString(KEY_USER); }
-    public String getSyntax() { return getString(KEY_SYNTAX); }
-    public String getTitle() { return getString(KEY_TITLE); }
-    public String getScrapeUrl() { return getString(KEY_SCRAPE_URL); }
-    public String getFullUrl() { return getString(KEY_FULL_URL); }
-    public long getDate() { return getLong(KEY_DATE);}
-    public long getSize() { return getLong(KEY_SIZE);}
-    public long getExpire() { return getLong(KEY_EXPIRE);}
-    public String getKey() { return getString(KEY_KEY);}
+    private JSONObject mJSON;
+
+    public Paste(JSONObject object){
+        this.mJSON = object;
+    }
+
+
+    public String getUser() { return mJSON.getString(KEY_USER); }
+    public String getSyntax() { return mJSON.getString(KEY_SYNTAX); }
+    public String getTitle() { return mJSON.getString(KEY_TITLE); }
+    public String getScrapeUrl() { return mJSON.getString(KEY_SCRAPE_URL); }
+    public String getFullUrl() { return mJSON.getString(KEY_FULL_URL); }
+    public long getDate() { return mJSON.getLong(KEY_DATE);}
+    public long getSize() { return mJSON.getLong(KEY_SIZE);}
+    public long getExpire() { return mJSON.getLong(KEY_EXPIRE);}
+    public String getKey() { return mJSON.getString(KEY_KEY);}
+    public void print(){
+        System.out.println(getKey());
+        System.out.println(getUser());
+        System.out.println(getTitle());
+        System.out.println(getScrapeUrl());
+        System.out.println(getFullUrl());
+        System.out.println(getDate());
+        System.out.println(getSize());
+        System.out.println(getExpire());
+    }
 }
 
 /*
